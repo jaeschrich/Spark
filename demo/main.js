9a5c42b13924ff43f8cc8054c08ee05c37864a0f
PyScripter <jaeschrich@gmail.com>
2012-08-20 17:45:29 -0400

    Modified demo/main.js

--- demo/main.js
+++ demo/main.js
@@ -6,7 +6,7 @@ function getPosts(){
     })
 }
 
-$.create('div', {'id': 'myDiv'})
+$.create('div', {'id': 'myDiv', 'class': 'test'})
 $.start('form', {'method': 'POST', 'action': '/upload'})
 $.create('input', {"type": 'text', 'name': 'msg'})
 $.create('button', {'type': 'submit'}, 'Submit')
